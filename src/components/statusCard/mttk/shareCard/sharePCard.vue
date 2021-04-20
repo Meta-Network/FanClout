1acafa1c37b4f4049c5a9e22fb3c03434f187268
LittleSound <464388324@qq.com>
2021-04-20 20:10:34 +0800

    替换无法使用的 $copyText

--- src/components/statusCard/mttk/shareCard/sharePCard.vue
+++ src/components/statusCard/mttk/shareCard/sharePCard.vue
@@ -79,6 +79,9 @@
 </template>
 
 <script>
+import useClipboard from 'vue-clipboard3'
+import { ElMessage } from 'element-plus'
+import i18n from '@/i18n'
 import { precision } from '@/utils/precisionConversion'
 
 export default {
@@ -99,6 +102,21 @@ export default {
     shareCard: {
       type: Boolean,
       default: false
+    }
+  },
+  setup () {
+    const global = i18n.global
+    const { toClipboard } = useClipboard
+    const copyCode = async function (text) {
+      try {
+        await toClipboard(text)
+        ElMessage.success(global.t('success.copy'))
+      } catch (err) {
+        ElMessage.error(global.t('error.copy'))
+      }
+    }
+    return {
+      copyCode
     }
   },
   computed: {
@@ -139,10 +157,7 @@ export default {
     copy (val, e) {
       if (e && e.preventDefault) e.preventDefault()
       else if (e && e.stopPropagation) e.stopPropagation()
-      this.$copyText(val).then(
-        () => this.$message({ showClose: true, message: this.$t('success.copy'), type: 'success' }),
-        () => this.$message({ showClose: true, message: this.$t('error.copy'), type: 'error' })
-      )
+      this.copyCode(val)
       return false
     },
     ref (val, e) {
