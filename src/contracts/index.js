2f27b2f948a9ee31defd92e4b2e4ba91dc3aa1a8
Garfield Lee <openpgp@550.moe>
2021-05-14 19:02:30 +0800

    :construction: 实现根据合约地址获取价格

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -18,8 +18,23 @@ export function getPairContract () {
   return mtbtContract
 }
 
-export async function checkMTBTIsToken0 () {
-  const contract = getPairContract()
+/**
+ * 获取交易对的合约实例
+ * @param {string} address 合约地址
+ * @returns 合约实例
+ */
+export function getPairContractByAddress (address) {
+  const provider = getBSCJsonRPCProvider()
+  const mtbtContract = new ethers.Contract(address, PAIR_ABI, provider)
+  return mtbtContract
+}
+
+/**
+ * 检查 Token 0 是否是 Meta Token
+ * @param {import('ethers').Contract} contract 合约实例
+ * @returns {Promise<boolean>} Token 0 是否是 Meta Token
+ */
+export async function checkMTBTIsToken0 (contract) {
   const token0 = await contract.token0()
   if (token0.toLowerCase() === MTBT_TOKEN.toLowerCase()) {
     return true
@@ -30,7 +45,7 @@ export async function checkMTBTIsToken0 () {
 export async function getMTBTPrice () {
   const contract = getPairContract()
 
-  const isToken0 = await checkMTBTIsToken0()
+  const isToken0 = await checkMTBTIsToken0(contract)
   const reserves = await contract.getReserves()
 
   const reserve0 = reserves._reserve0.toString()
